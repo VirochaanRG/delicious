758ca74d7492693a1852d8371a0146efd4ac4f02
Virochaan <39714123+VirochaanRG@users.noreply.github.com>
2022-05-16 16:38:11 -0400

    Importing Styling libraries and creating styling

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,57 @@
+import {useEffect, useState} from "react";
+import styled from "styled-components";
+import {Splide, SplideSlide} from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+
 function Popular() {
-  return (
-    <div>Popular</div>
-  )
+    const [popular, setPopular] = useState([]);
+
+
+    useEffect(() => {
+        getPopular();
+    }, []);
+    
+    const getPopular = async() => {
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+        const data = await api.json();
+        console.log(data);
+        setPopular(data.recipes)
+        console.log(data.recipes)
+    }
+
+    return (
+        <div>
+            {popular.map((recipe) => {
+                return (
+                    <Wrapper>
+                        <h3>Popular Picks</h3>
+                        {popular.map((recipe) =>{
+                            return (
+                                <Card>
+                                    <p>{recipe.title}</p>
+                                    <img src={recipe.image} alt={recipe.title}/>
+                                </Card>
+                            );
+                        })}
+                      <p>{recipe.title}</p>
+                    </Wrapper>  
+                );
+            })}
+        </div>
+    )
 }
 
+const Wrapper = styled.div`
+    margin: 4rem 0rem
+`;
+
+const Card = styled.div`
+    min-height: 25rem;
+    border-radius: 2rem;
+    overflow: hidden;
+
+    img {
+        border-radius: 2rem;
+    }
+`;
 export default Popular
