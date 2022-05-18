84e53b8eb38af95a9a128674d437fd5f019374c9
Virochaan <39714123+VirochaanRG@users.noreply.github.com>
2022-05-18 15:53:39 -0400

    Search Functionality implemented

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,24 @@
 import React, {useState} from 'react'
 import styled from 'styled-components'
 import {FaSearch} from "react-icons/fa"
+import {useNavigate} from "react-router-dom"
+
 
 function Search() {
   const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
   const submitHandler = (e) => {
       e.preventDefault();
+      navigate('/searched/'+input);
   };
 
+
+
     return (
-    <FormStyle>
+    <FormStyle onSubmit={submitHandler}>
         <div>
-            <FaSearch/>
+            <FaSearch></FaSearch>
             <input 
                 onChange = {(e)=> setInput(e.target.value)}
                 type="text" 
