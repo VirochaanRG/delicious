29e0aad98ee421b6adb201e7830cc2381397992f
Virochaan <39714123+VirochaanRG@users.noreply.github.com>
2022-05-18 10:48:08 -0400

    solving css problem and further styling

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -12,12 +12,19 @@ function Popular() {
     }, []);
     
     const getPopular = async() => {
-      const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
-      const data = await api.json();
-      console.log(data);
-      setPopular(data.recipes)
-      console.log(data.recipes)
-      
+
+      const check = localStorage.getItem('popular');
+
+      if (check) {
+        setPopular(JSON.parse(check));
+      }
+      else {
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+        const data = await api.json();
+        localStorage.setItem("popular", JSON.stringify(data.recipes))
+        setPopular(data.recipes)
+        console.log(data.recipes)
+      }
     }
 
     return (
@@ -33,7 +40,7 @@ function Popular() {
           }}>
             {popular.map((recipe) =>{
               return (
-                <SplideSlide>
+                <SplideSlide key = {recipe.ID}>
                   <Card>
                     <Gradient/>
                     <p>{recipe.title}</p>
@@ -51,16 +58,16 @@ function Popular() {
 
 const Wrapper = styled.div`
   margin: 4rem 0rem;
-  postion: absolute;
 `;
 
 const Card = styled.div`
   min-height: 25rem;
+  border-radius: 2rem;
   overflow: hidden;
-  postion: relative;
+  position: relative;
   img {
     border-radius: 2rem;
-    postion: absolute;
+    position: absolute;
     left: 0;
     width: 100%;
     height: 100%;
@@ -72,7 +79,7 @@ const Card = styled.div`
     left: 50%;
     bottom: 0%;
     transform: translate(-50%, 0%);
-    color: black;
+    color: white;
     width: 100%;
     text-align: center;
     font-weight: 600;
@@ -85,8 +92,8 @@ const Card = styled.div`
 `;
 
 const Gradient = styled.div`
-  z-index: 3
-  postion: absolute;
+  z-index: 3;
+  position: absolute;
   width: 100%;
   height: 100%;
   background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5))
